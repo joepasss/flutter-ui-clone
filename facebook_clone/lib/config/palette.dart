257f2e24629a76117ac096dffe2b033175d1afa6
Miller <farwes1180@gmail.com>
2022-03-02 14:56:05 +0900

    Add palette

---
+++ facebook_clone/lib/config/palette.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const Color scaffold = Color(0xFFF0F2F5);
+
+  static const Color facebookBlue = Color(0xff1777F2);
+
+  static const LinearGradient createRoomGradient =
+      LinearGradient(colors: [Color(0xff496AE1), Color(0xffCE48B1)]);
+
+  static const Color online = Color(0xff4BCB1F);
+
+  static const LinearGradient storyGradient = LinearGradient(
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
+      colors: [Color(0xff496AE1), Color(0xffCE48B1)]);
+}
