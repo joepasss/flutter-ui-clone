dbafb73718ee02e431e4f746e63de0a8b34f1231
Miller <farwes1180@gmail.com>
2022-03-08 16:20:23 +0900

    Add Stories widget

--- facebook_clone/lib/widgets/create_post_container.dart
+++ facebook_clone/lib/widgets/create_post_container.dart
@@ -1,6 +1,4 @@
 // ignore_for_file: deprecated_member_use, avoid_print
-
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 import '../models/user_model.dart';
