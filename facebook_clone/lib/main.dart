107ffd2f468c5a0759d5dc07c404ca9b91ed2898
Miller <farwes1180@gmail.com>
2022-03-10 17:20:14 +0900

    Add TabBar

--- facebook_clone/lib/main.dart
+++ facebook_clone/lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
         scaffoldBackgroundColor: Palette.scaffold,
       ),
-      home: const HomeScreen(),
+      home: const NavScreen(),
     );
   }
 }
